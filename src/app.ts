b3789b9097107dcf46193799d24587e73ec5b302
unknown <marcan.grab@gmail.com>
2023-11-24 13:44:36 +0100

    changed from socket.io to ws npm

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,13 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from "cors";
-import http from 'http';
-import { Server } from 'socket.io';
 
 dotenv.config();
 
 const app: Express = express();
-const server = http.createServer(app)
 const port = process.env.PORT;
 
 app.use(cors());
-
-const io = new Server(server, {
-    cors: {
-        origin: 'http://localhost:3000'
-    }
-})
-
-io.on('connection', (socket) => {
-    console.log(`⚡: ${socket.id} user just connected!`);
-    socket.on('disconnect', () => {
-      console.log('🔥: A user disconnected');
-    });
-});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
