1b64ab42e28280905527a4c7cc666dad13767d2b
unknown <marcan.grab@gmail.com>
2023-12-04 15:28:05 +0100

    added handling upgrade and connecting to ws server

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,14 @@ import WebSocket = require('ws');
 
 dotenv.config();
 
+function onSocketPreError(e: Error) {
+  console.log(e)
+} 
+
+function onSocketPostError(e: Error) {
+  console.log(e)
+} 
+
 const app: Express = express();
 const port = process.env.PORT;
 
@@ -27,5 +35,28 @@ const server = app.listen(port, () => {
 });
 
 server.on('upgrade', (req, socket, head) => {
-  
+  socket.on('error', onSocketPreError)
+
+  //preform auth
+  if (!!req.headers['BadAuth']) {
+    socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+    socket.destroy();
+    return
+  }
+
+  wss.handleUpgrade(req, socket, head, (ws)=>{
+    socket.removeListener('error', onSocketPreError)
+    wss.emit('connection', ws, req)
+  })
+})
+
+wss.on('connection', (ws,req) => {
+  ws.on('error', onSocketPostError);
+  ws.on('message', (msg, isBinary) => {
+    wss.clients.forEach((client)=>{
+      if(client.readyState === WebSocket.OPEN) {
+        client.send(msg,{binary:isBinary})
+      }
+    })
+  })
 })
